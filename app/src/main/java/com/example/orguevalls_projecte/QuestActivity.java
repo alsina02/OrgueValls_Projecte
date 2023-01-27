dac0243c0a939e0410e876208055af6020d72ff5
Jordi Alsina <jordi.alsinag02@gmail.com>
2023-01-27 16:45:13 +0100

    Commento

--- app/src/main/java/com/example/orguevalls_projecte/QuestActivity.java
+++ app/src/main/java/com/example/orguevalls_projecte/QuestActivity.java
@@ -10,27 +10,28 @@ import android.widget.TextView;
 
 public class QuestActivity extends AppCompatActivity {
 
-
+    // Variable declaration
     private Button ba11;
 
+    // Database handler declaration
     public DBHandler dbh = new DBHandler(this);
+
     @Override
-
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quest);
         setTitle(R.string.bQA);
 
+        // Links button with physical button
         ba11 = findViewById(R.id.botoA11);
 
         ba11.setOnClickListener(new View.OnClickListener() {
           @Override
-                      public void onClick(View view) {
-              Intent myIntent = new Intent(QuestActivity.this, A11.class);
-              QuestActivity.this.startActivity(myIntent);
-                }
-            });
+            public void onClick(View view) {
+                Intent myIntent = new Intent(QuestActivity.this, A11.class);
+                QuestActivity.this.startActivity(myIntent);
+            }
+        });
 
     }
 }
